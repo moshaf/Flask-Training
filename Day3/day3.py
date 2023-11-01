9669660a634b9aa2d8f6e528339f2066fca792e4
moshaf <mmehdi.shamsaei@gmail.com>
2023-11-01 13:23:04 -0400

    Flask-Day 3: Parsing the quesry and using the convertor

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,41 +1,27 @@
-from turtle import Turtle, Screen
-from Movement import Movement
-import time
-
-screen = Screen()
-screen.setup(500, 500)
-screen.bgcolor("black")
-screen.title("Snake game")
-screen.tracer(0)
-
-move = Movement()
-
-snake = []
-position = ((0, 0), (-20, 0), (-40, 0))
-
-for item in range(3):
-    segment = Turtle()
-    segment.penup()
-    segment.shape("square")
-    segment.color("white")
-    snake.append(segment)
-    segment.goto(position[item])
-
-game_on = True
-
-while game_on:
-    screen.update()
-    time.sleep(0.1)
-
-    for segment in range(len(snake) - 1, 0, -1):
-        new_x = snake[segment - 1].xcor()
-        new_y = snake[segment - 1].ycor()
-        snake[segment].goto(new_x, new_y)
-    # snake[0].fd(10)
-    screen.listen()
-    screen.onkey(move.move_right, "w")
+from flask import Flask
+import random
 
 
+app = Flask(__name__)
+number = random.randint(0,9)
+
+@app.route("/")
+def home():
+    return "<h1>Guess a number between 0 and 9: </h1>"
+
+@app.route("/<int:guess>")
+def compare(guess):
+    if guess > number:
+        return "<h1 style='color:red'>Your Guess is too high!</h1>"
+    elif number > guess:
+        return "<h1 style='color:blue'>Your Guess is too low!<h1>"
+    else:
+        return "<h1 style='color:green'>Your are correct!<h1>"
+
+@app.route("/<string:guess>")
+def not_valid(guess):
+    return f"<h1 style='color:red'>'{guess}' is not a number! please enter a number between 0 and 9<h1>"
 
 
-screen.exitonclick()
+if __name__ == "__main__":
+    app.run(debug=True)
